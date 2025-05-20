94d7ae8b1dbe4cdbf9a4343c4dd1d0e868a56d1f
Manvel <Manvelnavoan@gmail.com>
2025-05-20 19:11:05 +0300

    delete init num1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Задача 1
 
-Console.WriteLine("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 
